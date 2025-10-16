f55b634f645312efd5d06f9f35c495a2b7d3dafc
Yuna <52529145+YunaBlood@users.noreply.github.com>
2025-10-16 11:51:58 +0200

    Initialize gameboard and two players

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 require_relative "lib/board_game"
 require_relative "lib/player"
 require_relative "lib/game"
+
+game_board = BoardGame.new(Array.new(3) { Array.new(3) })
+
+bob = Player.new("Bob", "X", 1)
+david = Player.new("David", "O", 2)
